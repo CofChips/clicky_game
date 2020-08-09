06050303bf92dae9b0c098a426dc0744bb2ccf46
CofChips <qiwei.mod@gmail.com>
2020-08-08 20:32:48 -0700

    added image display functionality and wrapper for formatting

--- clicky_game/src/App.js
+++ clicky_game/src/App.js
@@ -1,12 +1,27 @@
 import React from "react";
 import Navbar from "./components/Navbar/index";
 import Jumbotron from "./components/Jumbotron/index";
+import Images from "./images.json";
+import ImageCard from "./components/ImageCard/index"
+import Wrapper from "./components/Wrapper/index"
+
+const images = Images.map((image)=>
+<ImageCard key={image.id.toString()}
+name = {image.name}
+image = {image.image}
+/>
+
+)
 
 function App() {
     return (
         <div>
             <Navbar />
             <Jumbotron />
+            <Wrapper>
+            {images}
+            </Wrapper>
+
         </div>
 
     );
