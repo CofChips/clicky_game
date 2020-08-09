7676bbcf7e891cb05ee24ffc6c2d9915c59d3d04
CofChips <qiwei.mod@gmail.com>
2020-08-09 14:57:56 -0700

    formatted jumbotron and added link to logo

--- clicky_game/src/components/Jumbotron/index.js
+++ clicky_game/src/components/Jumbotron/index.js
@@ -4,8 +4,8 @@ import "./style.css";
 function Jumbotron() {
     return (
         <div className="jumbotron jumbotron-fluid jumbo align-middle">
-            <div className="container text-center">
-                <h1 className="display-4 my-5 pt-5 clickytitle">Clicky Game!</h1>
+            <div className="container-fluid text-center wrap">
+                <h1 className="display-4 clickytitle">Clicky Game!</h1>
                 <p className="lead subhead">Click on an image to earn points, but don't click on any more than once!</p>
             </div>
         </div>
--- clicky_game/src/components/Navbar/index.js
+++ clicky_game/src/components/Navbar/index.js
@@ -1,13 +1,16 @@
 import React from "react";
+import { Link, useLocation } from "react-router-dom";
 import "./style.css";
 
 function Navbar(props) {
-
+    const location = useLocation();
     return (
         <nav className="navbar background fixed-top">
             <div className="row w-100 m-0">
                 <div className="col-4">
-                    <a className="navbar-brand brand" href="#">SF Giants Clicky Game!</a>
+                    <Link to="/" className={location.pathname === "/" ? "nav-link active" : "nav-link"}>
+                        <a className="navbar-brand brand" href="#">SF Giants Clicky Game!</a>
+                    </Link>
                 </div>
                 <div className="col-4 text-center">
                     <span className="navbar-text title">
