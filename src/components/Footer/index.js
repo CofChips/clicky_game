2c29563bfac20f318c48ec67d34f8663fc6b3ad6
CofChips <qiwei.mod@gmail.com>
2020-08-10 06:19:17 -0700

    Updated spacing for images

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,7 +3,7 @@ import "./style.css";
 
 function Footer() {
     return (
-        <footer className="footer">
+        <footer className="footer mt-4">
         <div className="container-fluid text-center height">
         </div>
     </footer>
