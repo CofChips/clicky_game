9c986e961592ec8277847e819fe54999b4f90058
CofChips <qiwei.mod@gmail.com>
2020-08-09 17:23:52 -0700

    Updates

---
+++ precache-manifest.f78e09af94079f103cd16de5569bb3d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c5ba13b542bbe4e8c1aa9a2d6a4e032",
+    "url": "/clicky_game/index.html"
+  },
+  {
+    "revision": "52b767c48acba0572551",
+    "url": "/clicky_game/static/css/main.2c02f848.chunk.css"
+  },
+  {
+    "revision": "aace3fdfffad6aeb7212",
+    "url": "/clicky_game/static/js/2.d0f2ee3a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clicky_game/static/js/2.d0f2ee3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52b767c48acba0572551",
+    "url": "/clicky_game/static/js/main.568639fc.chunk.js"
+  },
+  {
+    "revision": "86d1db1d5e50d888042c",
+    "url": "/clicky_game/static/js/runtime-main.612477c9.js"
+  }
+]);
